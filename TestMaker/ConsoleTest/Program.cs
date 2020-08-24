c8969354c4ce4e14e5dd8bf16d5feecfd38b2021
XeverDead <xever.dead.115@gmail.com>
2020-08-24 21:54:17 +0300

    Another task type and types tests in console

---
+++ TestMaker/ConsoleTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Lib;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = new Test("kek", null);
+
+            Console.WriteLine(test.Tasks.Count);
+        }
+    }
+}
---
+++ TestMaker/Lib/TaskTypes/MultipleChoice.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Lib.TaskTypes
+{
+    class MultipleChoice: ITask
+    {
+        public string Question { get; protected set; }
+        public List<string> Options { get; protected set; }
+        public List<string> RightAnswers { get; protected set; }
+        public int Time { get; protected set; }
+        public bool IsTimeLimited { get; protected set; }
+        public int Mark { get; protected set; }
+
+        public MultipleChoice(string question, List<string> options, List<string> rightAnswers, int mark)
+        {
+            Question = question;
+
+            if (options is null)
+            {
+                Options = new List<string>();
+            }
+            else
+            {
+                Options = new List<string>(options);
+            }
+
+            if (rightAnswers is null)
+            {
+                RightAnswers = new List<string>();
+            }
+            else
+            {
+                RightAnswers = new List<string>(rightAnswers);
+            }
+
+            Mark = mark;
+            Time = 0;
+            IsTimeLimited = false;
+        }
+
+        [JsonConstructor]
+        public MultipleChoice(int time, string question, List<string> options, List<string> rightAnswers, int mark)
+            : this(question, options, rightAnswers, mark)
+        {
+            if (time > 0)
+            {
+                Time = time;
+                IsTimeLimited = true;
+            }
+        }
+    }
+}
